a0dbb284598486f89a034acc5865f7592241efa0
jeppe sinkbaek <jeppest2@gmail.com>
2019-11-11 07:36:56 +0100

    Test implementation of clean procedure

--- ps_f.py
+++ ps_f.py
@@ -444,23 +444,34 @@ def create_pspectrum(y, t, freq_centre, half_width, resolution):
 
 
 def clean_procedure(t, y, n_iter, halfwidth, resolution, window=None, mph=1):
-    peak_freq = []
-    peak_power = []
+    # # Preparation
+    # Preload lists for storing found peaks
+    p_freq = []
+    p_power = []
+    # Get amount of frequency steps for nufft1
     steps = int((2 * halfwidth) / resolution)
+    # Make carbon copy of signal for manipulation
     y_copy = np.copy(y)
 
+    # # Loop
     for i in range(0, n_iter):
         t1 = tm.time()
+
+        # Get cyclic frequencies for nufft1
         freq = nufftpy.nufftfreqs(steps, df=resolution)
-        freq = freq[len(freq) // 2:-1]
+        freq = freq[len(freq) // 2:-1]  # Take only positive frequencies
+        # Call nufft1 to perform nufft calculation
         harmonic_content = nufftpy.nufft1(t, y_copy, steps, df=(resolution * 2 * np.pi))
         harmonic_content = harmonic_content[len(harmonic_content)//2:-1]
+        # Create window (if None) and apply to spectrum to only observe relevant area
         if window is None:
             window = range(0, len(harmonic_content))
         harmonic_content = harmonic_content[window]
         freq = freq[window]
+        # Calculate power
         spectral_power = harmonic_content.real ** 2 + harmonic_content.imag ** 2
 
+        # Detect peaks in power spectrum, and find power, frequency, alpha and beta values
         peaks = detect_peaks(spectral_power, mph=mph)
         peaks_power = spectral_power[peaks]
         peaks_alpha = harmonic_content.real[peaks]
@@ -471,34 +482,45 @@ def clean_procedure(t, y, n_iter, halfwidth, resolution, window=None, mph=1):
         # plt.plot(peaks_freq, peaks_power, 'r*', markersize=4)
         # plt.show()
 
+        # Find highest peak
         max_indx = np.argmax(peaks_power)
         max_freq = peaks_freq[max_indx]
         max_power = peaks_power[max_indx]
         max_alpha = peaks_alpha[max_indx]
         max_beta = peaks_beta[max_indx]
 
+        # Calculate harmonic signal corresponding to highest peak in power spectrum
         max_signal = (max_alpha * np.cos(2*np.pi*max_freq * t) + max_beta * np.sin(2*np.pi*max_freq * t)) * 2
 
         # plt.plot(t, y_copy, linewidth=0.5)
         # plt.plot(t, max_signal, '--', linewidth=0.3)
         # plt.show()
 
+        # Subtract calculated signal from y_copy and save the peak used
         y_copy -= max_signal
-        peak_power.append(max_power)
-        peak_freq.append(max_freq)
+        p_power.append(max_power)
+        p_freq.append(max_freq)
+
         t2 = tm.time()
         print(t2-t1)
 
+    # # Result
+    # Get cyclic frequencies for nufft1
     freq = nufftpy.nufftfreqs(steps, df=resolution)
     freq = freq[len(freq) // 2:-1]
+    # Call nufft1 to perform nufft calculation for unchanged signal y
     harmonic_content = nufftpy.nufft1(t, y, steps, df=(resolution * 2 * np.pi))
     harmonic_content = harmonic_content[len(harmonic_content) // 2:-1]
+    # Calculate spectral power
     spectral_power = harmonic_content.real ** 2 + harmonic_content.imag ** 2
+
+    # Plot power spectrum and peaks found by cleaning
     plt.plot(freq, spectral_power)
-    plt.plot(peak_freq, peak_power, 'r*', markersize=4)
+    plt.plot(p_freq, p_power, 'r*', markersize=4)
     plt.show()
 
-    writer('clean_peaks', peak_freq, peak_power)
+    # Save peaks found by cleaning in .dat file
+    writer('clean_peaks', p_freq, p_power)
 
 
 def pwriter(filename, freq, p, alpha, beta):
