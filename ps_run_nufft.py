822586f8d0ca4691d4f9a2f51d392f2391cd4afb
jeppe sinkbaek <jeppest2@gmail.com>
2019-11-14 23:45:09 +0100

    testing with old spectrum

--- ps_run_nufft.py
+++ ps_run_nufft.py
@@ -11,10 +11,15 @@ print('What is the filename? (No file extension)')
 inpt1 = input()
 
 data = ps_f.reader(inpt1)
+spectrum_old = ps_f.reader('spectrum_star2_old')
 
 # # Pull variables from data
 time = data[0].ravel()  # ravel() is used to change shape from (n, 1) to (n, )
 flux = data[1].ravel()
+freq_old = spectrum_old[0].ravel()
+spectral_power_old = spectrum_old[1].ravel()
+alpha_old = spectrum_old[2].ravel()
+beta_old = spectrum_old[3].ravel()
 
 # # Used to indicate which window, modulus and scaling should be used
 dataset = 1  # 0 = sun, 1 = star2, 2 = nuindi
@@ -24,7 +29,7 @@ muHz = 0.000001  # Variable for converting to microHz
 
 
 # # Frequency calculation
-resolution = 0.001 * muHz  # 0.01 normal
+resolution = 0.01 * muHz  # 0.01 normal
 halfwidth_set = [6000, 1000, 600]  # sun, star2, nuindi
 # halfwidth = 6000 * muHz  # sun
 halfwidth = halfwidth_set[dataset] * muHz
@@ -38,10 +43,18 @@ result = nufftpy.nufft1(time, flux, steps, df=(resolution * 2 * math.pi))
 res_pos = result[len(result)//2:-1]
 
 spectral_power = res_pos.real ** 2 + res_pos.imag ** 2
-plt.plot(spectral_power)
+
+plt.figure()
+plt.plot(freq / muHz, spectral_power)
+plt.plot(freq_old / muHz, spectral_power_old, '--', linewidth=0.5)
+plt.plot(freq / muHz, 4*spectral_power, 'b-.', linewidth=0.5)
 # plt.plot(res_pos.real, 'r--', linewidth=0.3)
 # plt.plot(res_pos.imag, 'b--', linewidth=0.3)
+plt.legend(['nufft', 'sine-cosine fitting', 'nufft * 4'])
+plt.xlabel('Frequency [muHz]')
+plt.ylabel('Power')
 plt.show()
+
 # ps_f.writer('spectrum', freq, spectral_power, res_pos.real, res_pos.imag)
 
 
