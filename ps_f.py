8f37a53240e1b88ba499ceaf490ef61a81751dc9
Jeppe Thomsen <jeppest2@gmail.com>
2020-10-20 09:57:48 +0200

    cuda code variation

--- ps_f.py
+++ ps_f.py
@@ -7,7 +7,6 @@ import numpy as np
 import math
 import os
 import time as tm
-import nufftpy
 from detect_peaks import detect_peaks
 import matplotlib.pyplot as plt
 
@@ -155,8 +154,123 @@ def create_pspectrum(y, t, freq_centre, half_width, resolution, chunk_size=100, 
     return results
 
 
+def create_pspectrum_cuda(y, t, freq_centre, half_width, resolution, chunk_size=100, dtype=np.double):
+    # # Uses cupy (cuda numpy) instead of numpy # #
+    import cupy as cu
+    pi = math.pi
+    t1 = tm.time()
+
+    # # # # Prepare input for use # # # #
+    # Convert input to cupy array (if not already)
+    y = cu.asarray(y)
+    t = cu.asarray(t)
+
+    # Change to angular frequencies (assumes input is in cyclic frequencies)
+    freq_centre[:] = [x * (2 * pi) for x in freq_centre]
+    half_width = half_width * 2 * pi
+    resolution = resolution * 2 * pi
+
+    # Data mean subtraction, to reduce potentially constant elements
+    y_mean = cu.mean(y, dtype=dtype)
+    y = y - y_mean
+    t = t - cu.min(t)  # To get first element in t to be 0
+
+    # # # # Prepare for for-loop # # # #
+    # Preload results list
+    results = [None] * len(freq_centre)
+
+    # Set amount of steps (might be subject to rounding error)
+    step_amnt = int((2 * half_width) / resolution)
+
+    # # # # Run for loop for all frequencies indicated by the list freq_centre # # # #
+    for k in range(0, len(freq_centre)):
+        freq_centre_current = freq_centre[k]
+        # Create frequency steps
+        freq = cu.linspace(freq_centre_current - half_width, freq_centre_current + half_width, step_amnt, dtype=dtype)
+        # Reshape freq in order to do matrix multiplication
+        freq = cu.reshape(freq, (len(freq), 1))
+        lent = len(t)
+
+        # # Prepare to calculate sine and cosine function parts of power spectrum estimate # #
+        sin = np.zeros(step_amnt)
+        cos = np.zeros(step_amnt)
+        sin2 = np.zeros(step_amnt)
+        cos2 = np.zeros(step_amnt)
+        sincos = np.zeros(step_amnt)
+
+        freq = cu.ascontiguousarray(freq)
+        t = cu.ascontiguousarray(t)
+        # Recurrence sine and cosine difference product
+        s_diff = cu.sin(resolution * t, dtype=dtype)
+        c_diff = cu.cos(resolution * t, dtype=dtype)
+
+        # # # # # Calculation matrices # # # # # # # # # # # # # # # # # # # # # # # # # # #
+        # use [c0, s0][c_diff, s_diff; -s_diff, c_diff]  (inverted in calc for .T)         #
+        # Recurrence based on s_m = c_(m-1)*sin(deltaf * t) + s_(m-1)*cos(deltaf * t) and  #
+        # c_m = c_(m-1)*cos(deltaf * t) - s_(m-1)*sin(deltaf * t) from T. Ponman 1981      #
+        # # # # # # # # # #  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+        # # Prepare linear operation matrix with initial operation # #
+        calc_base = cu.array([[c_diff, -s_diff], [s_diff, c_diff]]).T
+        calc_mat = cu.zeros((chunk_size, lent, 2, 2))
+        calc_mat[0, :, :, :] = calc_base
+        # # Generate linear operation matrices for recurrence multiplication # #
+        for i in range(1, chunk_size):
+            calc_mat[i, :, :, :] = cu.matmul(calc_mat[i - 1, :, :, :], calc_base)
+
+        # Convert large matrix arrays to contiguous arrays
+        calc_mat = cu.ascontiguousarray(calc_mat)
+
+        # # Calculate sine and cosine function parts of power spectrum estimation # #
+        for i in range(0, step_amnt, chunk_size):
+            end = i + chunk_size
+            if end > step_amnt:
+                dif = end - step_amnt
+                calc_mat = cu.asnumpy(calc_mat)
+                calc_mat = np.delete(calc_mat, range(chunk_size - dif, chunk_size), 0)
+                calc_mat = cu.ascontiguousarray(calc_mat)
+                end = step_amnt
+                chunk_size = end - i
+
+            print('Current chunk ', i, ':', end, ' of ', step_amnt)
+            # Original point calculation
+            s0 = cu.sin(freq[i] * t, dtype=dtype)
+            c0 = cu.cos(freq[i] * t, dtype=dtype)
+            # Sine/cosine vector initialization (for matmul calculation, see c0, s0 before loop)
+            trig_vec = cu.zeros((1, lent, 1, 2))
+            trig_vec[0, :, 0, 0] = c0
+            trig_vec[0, :, 0, 1] = s0
+            trig_vec = cu.repeat(trig_vec, chunk_size, axis=0)
+            # Matrix calculations
+            matrix_result = cu.matmul(trig_vec, calc_mat)
+            sin_temp = matrix_result[:, :, 0, 1]
+            cos_temp = matrix_result[:, :, 0, 0]
+
+            # # Sum and save results # #
+            sin[i:end] = cu.sum(y * sin_temp, 1).get()
+            cos[i:end] = cu.sum(y * cos_temp, 1).get()
+            sin2[i:end] = cu.sum(sin_temp ** 2, 1).get()
+            cos2[i:end] = cu.sum(cos_temp ** 2, 1).get()
+            sincos[i:end] = cu.sum(sin_temp * cos_temp, 1).get()
+
+        # # # # Calculate alpha and beta components of spectrum, and from them, power of spectrum # # # #
+        alpha = (sin * cos2 - cos * sincos) / (sin2 * cos2 - sincos**2)
+        beta = (cos * sin2 - sin * sincos) / (sin2 * cos2 - sincos**2)
+        power = alpha**2 + beta**2
+
+        # # # # Last loop steps # # # #
+        # Convert frequency back to cyclic units
+        freq = freq.get() / (2 * pi)
+        # Save data in results
+        results[k] = [freq, power, alpha, beta]
+    t2 = tm.time()
+    print('Total time elapsed create_pspectrum_cuda: ', t2-t1, ' seconds')
+    return results
+
+
 def clean_procedure(t, y, n_iter, halfwidth, resolution, window=None, mph=1):
     # # Preparation
+    import nufftpy
     # Preload lists for storing found peaks
     p_freq = []
     p_power = []
--- ps_run_sincosfit.py
+++ ps_run_sincosfit.py
@@ -1,35 +1,36 @@
 import ps_f
 import matplotlib.pyplot as plt
+import cupy as cu
 import numpy as np
 import time as tm
 # import as_f
 plt.rcParams.update({'font.size': 25})  # Changes font size for all plots
 
 # # Pull variables from data
-# inpt1 = 'signal_golf'
-inpt1 = 'signal_betelgeuse'
+inpt1 = 'signal_golf'
+# inpt1 = 'signal_betelgeuse'
 
 data = ps_f.reader(inpt1)
-time = data[0].ravel() / 86400  # data[0] is in seconds
+time = data[0].ravel()  # (/ 86400 for betelgeuse)  # data[0] is in seconds
 flux = data[1].ravel()
 
 # # Frequency Conversion
-muFreqUnit = 0.000001 * 86400  # Variable for converting to microHz (or micro rev per day)
+muFreqUnit = 0.000001  # (* 86400 for betelgeuse)  # Variable for converting to microHz (or micro rev per day)
 
 # # Frequency calculation
 resolution = 0.01 * muFreqUnit  # 0.01 normal
-halfwidth = 20 * muFreqUnit
+halfwidth = 6000 * muFreqUnit  # (betelgeuse 20)
 steps = int((2 * halfwidth) / resolution)
 
-# # Spectrum calculation from sine cosine least squares fitting
-results = ps_f.create_pspectrum(flux, time, [20.02 * muFreqUnit], halfwidth, resolution, chunk_size=50,
-                                dtype=np.longdouble)[0]
+# # Spectrum calculation from sine cosine least squares fitting (betelgeuse 20.02 * muFreqUnit)
+results = ps_f.create_pspectrum_cuda(flux, time, [6005 * muFreqUnit], halfwidth, resolution, chunk_size=100,
+                                     dtype=cu.double)[0]
 freq, spectral_power = results[0], results[1]
 
 plt.plot(freq, spectral_power)
 plt.ylim([-10, 4*1.34*10**11])
 plt.xlim([0.04, 1.7])
 plt.ylabel('Power')
-plt.xlabel('1/days')
+plt.xlabel('muHz')
 plt.legend(['sine/cosine fitting'])
 plt.show()
