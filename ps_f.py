396f165bfc9110b0672e92f5ea4df5cbe3b9aa7c
jeppe sinkbaek <jeppest2@gmail.com>
2020-11-02 10:30:53 +0100

    update ps_f.optimal_width to be more functional (still needs work)

--- ps_f.py
+++ ps_f.py
@@ -23,7 +23,7 @@ def reader(fname):
     return np.array_split(arr, len(arr[0, :]), axis=1)
 
 
-def optimal_resolution(t, initial_resolution, sfreq, half_width=None):
+def optimal_resolution(t, initial_resolution, sfreq, half_width=None, chunk_size=100):
     """
     Finds a more optimal frequency resolution to reduce the amount of noise introduced
     in the spectrum. Does it by simulating a sine function sampled similarly to the data set
@@ -35,7 +35,9 @@ def optimal_resolution(t, initial_resolution, sfreq, half_width=None):
         half_width = sfreq - sfreq/100
     yt = np.sin(2*math.pi*sfreq*t)
     # Initial
-    pres = create_pspectrum.numpy(yt, t, freq_centre=sfreq, half_width=half_width, resolution=initial_resolution)
+    #pres = create_pspectrum.numpy(yt, t, freq_centre=[sfreq], half_width=half_width, resolution=initial_resolution,
+    #                              chunk_size=chunk_size)
+    pres = create_pspectrum.nufftpy(yt, t, half_width=half_width, resolution=initial_resolution)
     freq_i = pres[0]
     power_i = pres[1]
     # Find area under the curve (sum the power spectrum)
@@ -46,9 +48,11 @@ def optimal_resolution(t, initial_resolution, sfreq, half_width=None):
     best_p = power_i
 
     # # # Loop to examine # # # #
-    resolution = np.linspace(0.5*initial_resolution, 1.5*initial_resolution, 100)
+    resolution = np.linspace(0.90*initial_resolution, 2.5*initial_resolution, 10)
     for current_res in resolution:
-        pres = create_pspectrum.numpy(yt, t, freq_centre=sfreq, half_width=half_width, resolution=current_res)
+        pres = create_pspectrum.numpy(yt, t, freq_centre=[sfreq], half_width=half_width, resolution=current_res,
+                                      chunk_size=chunk_size)[0]
+        #  pres = create_pspectrum.nufftpy(yt, t, half_width=half_width, resolution=current_res)
         current_power = pres[1]
         current_area = np.sum(current_power)
         if current_area < best_area:
@@ -59,8 +63,10 @@ def optimal_resolution(t, initial_resolution, sfreq, half_width=None):
 
     print('Best resolution: ', best_res)
     print('Initial resolution: ', initial_resolution)
-    plt.plot(freq_i, power_i, '--')
-    plt.plot(best_freq, best_p)
+    plt.plot(freq_i/0.000001, power_i, '--')
+    plt.plot(best_freq/0.000001, best_p)
+    plt.show()
+    return best_res
 
 
 
